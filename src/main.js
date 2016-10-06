9ce395335796c6c1f8cad8604802a603d502ac48
tg <tgdevjs@gmail.com>
2016-10-06 14:41:52 -0400

    Added task list and initial styles.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,30 @@
 import React, {Component} from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 module.exports = React.createClass({
+  getInitialState() {
+    return {
+      tasks: ['task1','task2','task3']
+    }
+  },
+  renderList(tasks){
+    return tasks.map( (task) => {
+      return (
+        <View key={task} style={styles.task}>
+          <Text>
+            {task}
+          </Text>
+        </View>
+      );
+    })
+  },
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Main</Text>
+      <View style = {styles.container}>
+        <Text style = {styles.header}>
+          Main
+        </Text>
+        {this.renderList(this.state.tasks)}
       </View>
     )
   }
@@ -17,8 +32,19 @@ module.exports = React.createClass({
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
+  },
+  header: {
+    margin: 30,
+    marginTop: 40,
+    fontSize: 18,
+    textAlign: 'center',
+  },
+  task: {
+    height: 60,
+    borderWidth: 1,
+    borderColor: 'black',
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
   },
 });
