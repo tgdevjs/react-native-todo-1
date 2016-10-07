a2b79bc34d5126c59c998d4871fe7db6c3d88b8a
tg <tgdevjs@gmail.com>
2016-10-07 11:06:27 -0400

    Add task with TextInput.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TextInput, View, } from 'react-native';
 
 module.exports = React.createClass({
   getInitialState() {
     return {
+      task: '',
       tasks: ['task1','task2','task3']
     }
+  },
+  addTask() {
+    let tasks = this.state.tasks.concat([this.state.task]);
+    this.setState({tasks});
+    console.log('AddTask: ',this.state.task);
+    console.log('  Tasks: ',this.state.tasks);
   },
   renderList(tasks){
     return tasks.map( (task) => {
@@ -24,6 +31,14 @@ module.exports = React.createClass({
         <Text style = {styles.header}>
           Main
         </Text>
+        <TextInput
+          style={styles.input}
+          placeholder='Add a task...'
+          onChangeText={(text)=>{
+            this.setState({task: text});
+          }}
+          onEndEditing={()=>{this.addTask()}}
+        />
         {this.renderList(this.state.tasks)}
       </View>
     )
@@ -39,6 +54,14 @@ const styles = StyleSheet.create({
     marginTop: 40,
     fontSize: 18,
     textAlign: 'center',
+  },
+  input: {
+    height: 60,
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: 'black',
+    textAlign: 'center',
+    margin: 10
   },
   task: {
     height: 60,
